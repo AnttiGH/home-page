94c6525b10e9eb1f9735b6e4318fbbb55b7893a9
AnttiGH <antti.va.nygren@gmail.com>
2025-12-06 11:52:56 +0200

    Changes

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.get('/ping', (req, res) => {
 });
 
 app.get('/', (req, res) => {
-  res.send('Home page of Antti. The server running with new changes!');
+  res.send('Home page of Antti. The server running with new changes!2');
 });
 
 app.listen(PORT, '0.0.0.0', () => {
