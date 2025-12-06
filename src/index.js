2844d0e5db773c1c79cac09a628730c5bdca1802
AnttiGH <antti.va.nygren@gmail.com>
2025-12-06 09:01:47 +0200

    Change index file title

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.get('/ping', (req, res) => {
 });
 
 app.get('/', (req, res) => {
-  res.send('Home page server running with new changes!');
+  res.send('Home page of Antti. The server running with new changes!');
 });
 
 app.listen(PORT, '0.0.0.0', () => {
